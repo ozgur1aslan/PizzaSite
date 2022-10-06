96ad041a97b24fafd982adf497f0fe7f6fe1c90e
ozgur1aslan <aslanozgur176@gmail.com>
2022-10-06 14:04:14 +0300

    Configured connection string for deployment.

---
+++ PizzaSite/Migrations/20221005223640_herokuPostgres.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaSite.Migrations
+{
+    public partial class herokuPostgres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
